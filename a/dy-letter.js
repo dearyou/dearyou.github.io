53cf70e15c5d3dad95d611f1daf7068dea454c42
Zach <zbecker10@gmail.com>
2015-05-25 00:07:27 -0400

    DY,

--- a/dy-letter.js
+++ a/dy-letter.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
     var id = Object.keys(dy.getJsonFromUrl())[0]
-    $.getJSON("https://dearyou.cloudant.com/posts/_design/posts/_view/byID?" + $.param({
-        key: "\"" + id + "\"",
-        include_docs: true
-    }), function (data) {
-        if (data.rows.length > 0) {
-            data.rows.forEach(function(row) {
-                dy.createDocs(row.doc)
-            })
-        } else {
-            $(".posts").html("<div class='no_results'>Sorry, that post does not exist! Maybe check out the 'Search' link?</div>")
-        }
-        $(".timeago").timeago()
-    })
+    if (id) {
+        $.getJSON("https://dearyou.cloudant.com/posts/_design/posts/_view/byID?" + $.param({
+            key: "\"" + id + "\"",
+            include_docs: true
+        }), function (data) {
+            if (data.rows.length > 0) {
+                data.rows.forEach(function(row) {
+                    dy.createDocs(row.doc)
+                })
+            } else {
+                $(".posts").html("<div class='post'><div class='inner_post'><h3>Sorry, that post does not exist!</h3></div></div>")
+            }
+            $(".timeago").timeago()
+        })
+    } else {
+        $(".posts").html("<div class='post'><div class='inner_post'><h3>Try clicking \"Search\" ;)</h3></div></div>")
+    }
 })
--- a/dy-tag.js
+++ a/dy-tag.js
@@ -14,12 +14,12 @@ $(document).ready(function() {
                     dy.createDocs(row.doc)
                 })
             } else {
-                $(".posts").html("<div class='no_results'>Sorry, there are no posts with that tag. Click the 'Search' link!</div>")
+                $(".posts").html("<div class='post'><div class='inner_post'><h3>Sorry, there are no posts with that tag.</h3></div></div>")
             }
             $(".timeago").timeago()
         })
     } else {
-        $(".posts").html("<h3>Try clicking \"Search\" ;)</h3>")
+        $(".posts").html("<div class='post'><div class='inner_post'><h3>Try clicking \"Search\" ;)</h3></div></div>")
     }
     $(document).on("submit", ".tag_form", function() {
         event.preventDefault()
